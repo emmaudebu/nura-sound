28f8e7a93779b42842473c08f0db17f0c6a28fba
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-06-30 13:16:29 +0100

    header

--- my-project/src/Components/Header.jsx
+++ my-project/src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../assets/images/svg.png";
+import menu from "../assets/images/icons/menu.svg";
 
 const Header = () => {
   return (
     <>
-      <div className=" w-[100%] h-[100vh] bg-hero-bg text-white main-padding bg-contain md:bg-cover   bg-no-repeat ">
+      <div className="  w-[100vw] h-[50vh] md:w-[100%] md:h-[100vh] bg-hero-bg text-white bg-cover main-padding bg-contain md:bg-cover   bg-no-repeat ">
         <header className=" flex items-center justify-between py-5">
           <nav className=" flex items-center gap-5 ">
             <img src={logo} />
@@ -15,7 +16,12 @@ const Header = () => {
               <li>Support</li>
             </ul>
           </nav>
-          <span className=" cursor-pointer">Cart(0)</span>
+          <div className=" flex items-center gap-4 text-white">
+            <span className=" cursor-pointer">Cart(0)</span>
+            <span className=" cursor-pointer block md:hidden ">
+              <img src={menu} alt="" className="text-lg text-white" />
+            </span>
+          </div>
         </header>
       </div>
     </>
