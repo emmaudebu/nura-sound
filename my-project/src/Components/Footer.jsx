867b2b459ada5031e29007d5546912e096ab60a6
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-07-08 13:04:38 +0100

    done

--- my-project/src/Components/Footer.jsx
+++ my-project/src/Components/Footer.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/images/svg.png";
 
 export const Footer = () => {
   return (
-    <div div className=" bg-black px-3  md:px-[64px]">
-      <div className="  bg-black text-white pt-[5.6rem]  hidden flex-wrap  md:flex :flex-row gap-11 md:gap-[9rem] pb-10">
+    <div div className=" bg-black px-3  md:px-[20px]">
+      <div className="  bg-black text-white pt-[5.6rem]  hidden flex-wrap  md:flex :flex-row gap-5 md:gap-[6rem] pb-10">
         <div className=" w-[21rem] pr-[2.5rem]">
           <img src={logo} alt="" className="pb-5" />
           <p>
